13d04c82b2bb20f6cae9012321caf059ce6486f3
Neeraj Kumar <sainineeraj1234@gmail.com>
2024-10-14 17:47:18 +0530

    Fixed

--- classroom/settings/base.py
+++ classroom/settings/base.py
@@ -90,6 +90,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "classroom.wsgi.application"
+ASGI_APPLICATION = "classroom.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
