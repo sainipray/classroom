ec695ad6b7f0aa0b52cbb91d8c51cf45b662bfb1
Neeraj Kumar <sainineeraj.1234@gmail.com>
2024-10-18 17:03:25 +0530

    Fixed

--- apps/user/student_views.py
+++ apps/user/student_views.py
@@ -5,12 +5,10 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from .models import CustomUser, Roles
-from .serializers import (
-    StudentSerializer, )
-from .student_serializers import StudentUserProfileSerializer
+from .student_serializers import StudentUserProfileSerializer, StudentProfileSerializer
 
 
-class StudentProfileViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet):
+class StudentProfileViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
     queryset = CustomUser.objects.filter(role=Roles.STUDENT).exclude(student__isnull=True)
     serializer_class = StudentUserProfileSerializer
 
@@ -22,11 +20,13 @@ class StudentProfileViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, 
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
-    def update(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
         student = instance.student
-        serializer = StudentSerializer(student, data=request.data, partial=partial)
+        serializer = StudentProfileSerializer(student, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+        for attr, value in serializer.validated_data.items():
+            setattr(student, attr, value)
+        student.save()
         return Response({"message": "Student information Updated"}, status=status.HTTP_200_OK)
