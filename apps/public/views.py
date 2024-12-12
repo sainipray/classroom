9f443a4c0e1568f096597ed0ae2e925af73faf79
Neeraj Kumar <sainineeraj1234@gmail.com>
2024-12-12 12:37:17 +0530

    Fixed

--- apps/public/views.py
+++ apps/public/views.py
@@ -13,11 +13,13 @@ from apps.test_series.models import TestSeries
 class PublicCategoryViewSet(ReadOnlyCustomResponseMixin):
     serializer_class = PublicCategorySerializer
     queryset = Category.objects.filter(status=1)
+    permission_classes = [AllowAny]
 
 
 class PublicSubjectViewSet(ReadOnlyCustomResponseMixin):
     serializer_class = PublicSubjectSerializer
     queryset = Subject.objects.filter(is_active=True)
+    permission_classes = [AllowAny]
 
 
 class PublicTestSeriesViewSet(ReadOnlyCustomResponseMixin):
